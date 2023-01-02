a883fe25747cce07076a663c0899fe184f703474
Aditya Trivedi <adityakuldeeptrivedi@gmail.com>
2023-01-02 14:57:50 +0530

    Delete Transaction Functionality

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
         accentColor: Colors.amber,
+        // errorColor: Colors.red,
         fontFamily: 'Quicksand',
         textTheme: ThemeData.light().textTheme.copyWith(
               headline6: TextStyle(
@@ -98,6 +99,14 @@ class _MyHomePageState extends State<MyHomePage> {
         });
   }
 
+  void _deleteTransaction(String id) {
+    setState(() {
+      _userTransactions.removeWhere((tx) {
+        return tx.id == id;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -116,15 +125,18 @@ class _MyHomePageState extends State<MyHomePage> {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               Chart(_recentTransactions),
-              Card(
-                color: Colors.blue,
-                elevation: 5,
-                child: Container(
-                  width: double.infinity,
-                  child: Text('CHART!'),
-                ),
+              // Card(
+              //   color: Colors.blue,
+              //   elevation: 5,
+              //   child: Container(
+              //     width: double.infinity,
+              //     child: Text('CHART!'),
+              //   ),
+              // ),
+              TransactionList(
+                _userTransactions,
+                _deleteTransaction,
               ),
-              TransactionList(_userTransactions),
             ]),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -4,13 +4,14 @@ import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
+  final Function deleteTx;
 
-  TransactionList(this.transactions);
+  TransactionList(this.transactions, this.deleteTx);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
+      height: 520,
       child: transactions.isEmpty
           ? Column(
               children: <Widget>[
@@ -52,6 +53,11 @@ class TransactionList extends StatelessWidget {
                     ),
                     subtitle: Text(
                         DateFormat.yMMMd().format(transactions[index].date)),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete),
+                      color: Theme.of(context).errorColor,
+                      onPressed: () => deleteTx(transactions[index].id),
+                    ),
                   ),
                 );
               },
